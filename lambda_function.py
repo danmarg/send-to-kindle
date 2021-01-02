5feb4dbc904a3da8f4a091eaad1a8bcd5fa21e99
Daniel Margolis <dan@af0.net>
2021-01-02 19:40:00 +0100

    Encode Unicode. Allow testing the lambda handler.

--- lambda_function.py
+++ lambda_function.py
@@ -6,6 +6,7 @@ import urllib
 from base64 import b64encode, b64decode
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from html.entities import codepoint2name
 from urllib import parse
 
 import tldextract
@@ -17,6 +18,17 @@ RE = r'''((?:http|https)://(?:[\w_-]+(?:(?:\.[\w_-]+)+))(?:[\w.,@?^=%&:/~+#-]*[\
 ALLOWED_DOMAINS = ['af0.net', 'kindle.com']
 
 USERAGENT  = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+
+
+def decode_to_entity(s):
+    t = ''
+    for i in s:
+        if ord(i) in codepoint2name:
+            name = codepoint2name.get(ord(i))
+            t += '&' + name + ';'
+        else:
+            t += i
+    return t
 
 def fetch_image_to_b64(url):
     req = urllib.request.Request(url, headers={'User-Agent': USERAGENT})
@@ -55,7 +67,7 @@ def fetch_and_format(url, fetch_img=True):
     art = Article(url, config=cfg)
     art.download()
     art.parse()
-    title = art.title
+    title = decode_to_entity(art.title)
     # TODO: Newspaper3k parses the same author multiple times. Fix!
     author = art.authors[0] if art.authors else ''
     publish_date = art.publish_date.strftime('%B %d %Y') if art.publish_date else ''
@@ -75,7 +87,7 @@ def fetch_and_format(url, fetch_img=True):
     {text}
     </body>
     </html>'''
-    return title, doc
+    return art.title, doc
 
 def html_as_mime_attachment(title, html):
     filename = ''.join([c for c in title
@@ -83,23 +95,21 @@ def html_as_mime_attachment(title, html):
     attach = MIMEText(html, 'html', 'utf-8')
     attach.add_header('Content-Disposition', 'attachment',
             filename=filename + '.html')
-    attach.add_header('Content-Type', 'text/html; charset=UTF-8')
     return attach
 
-def lambda_handler(event, context):
-    rec = json.loads(event['Records'][0]['Sns']['Message'])
-    dst = rec['mail']['destination']
-    headers = rec['mail']['headers']
-    body = b64decode(rec['content'])
-    msg = email.message_from_bytes(body)
+def lambda_handler(event=None, context=None, dst=None, urls=[], pdfs=[], do_mail=True):
+    if event:
+        rec = json.loads(event['Records'][0]['Sns']['Message'])
+        dst = rec['mail']['destination']
+        headers = rec['mail']['headers']
+        body = b64decode(rec['content'])
+        msg = email.message_from_bytes(body)
 
-    urls = []
-    pdfs = []
-    for part in msg.walk():
-        if part.get_content_type() == 'text/plain':
-            urls += re.findall(RE, part.as_string())
-        elif part.get_content_type() == 'application/pdf':
-            pdfs.append(part)
+        for part in msg.walk():
+            if part.get_content_type() == 'text/plain':
+                urls += re.findall(RE, part.as_string())
+            elif part.get_content_type() == 'application/pdf':
+                pdfs.append(part)
 
     if not urls and not pdfs:
         print('No URLs or PDFs!')
@@ -133,14 +143,18 @@ def lambda_handler(event, context):
     print('Article:', urls[0])
     print('Send to:', dst)
 
-    msg = MIMEMultipart('related')
+    msg = MIMEMultipart('mixed')
     msg['Subject'] = subj
     msg['From'] = '<kindle@x.af0.net>'
     msg['To'] = dst
-    msg.preamble = 'Multipart message.\n'
+    msg.attach(MIMEText('Multipart message.'))
+
     for a in attach:
       msg.attach(a)
 
+    if not do_mail:
+        print(msg)
+        return
 
     client = boto3.client('ses' )    
 
